a894a802408fb391c0abc37db5ec816787afe813
ljaviertovar <luisjavier_tovar@outlook.com>
2021-04-21 22:36:06 -0500

    :tada: Create Router

---
+++ src/components/auth/NewAccount.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NewAccount = () => {
+    return ( 
+        <h1>New account</h1>
+    );
+}
+ 
+export default NewAccount;
---
+++ src/components/proyects/Proyects.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Proyects = () => {
+    return ( 
+        <h1>Proyects</h1>
+     );
+}
+ 
+export default Proyects;
