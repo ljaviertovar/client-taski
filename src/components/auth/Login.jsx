8d715971aa30bc4cf1f8a62e38775f791c3fe2f5
ljaviertovar <luisjavier_tovar@outlook.com>
2021-04-21 23:09:12 -0500

    :hammer: Create Form in Login component

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,8 +1,49 @@
 import React from 'react';
 
 const Login = () => {
+
+    const onChange=() =>{
+
+    }
+
     return ( 
-        <h1>Login</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Login</h1>
+
+                <form>
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Your Email"
+                            id="email"
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Your Password"
+                            id="password"
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="camplo-form">
+                        <input
+                            type="submit"
+                            value="Login"
+                            className="btn btn-primario btn-block"
+                        />
+                    </div>
+                </form>
+            </div>
+
+        </div>
      );
 }
  
