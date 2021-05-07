d0a75f3a7eb88f67943823880af6c5cc9250a970
ljaviertovar <luisjavier_tovar@outlook.com>
2021-05-07 18:00:46 -0500

    :construction: Create context

---
+++ src/context/projects/projectState.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import projectContext from './projectContex';
+import projectReducer from './projectReducer'
+
+const projectState = props => {
+
+    const initialState = {
+        newProject: false
+    };
+
+    //dispatch to exect actions
+    const 
+
+}
